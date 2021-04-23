ee3efec6b9850d9052674703d9338b517259cccc
revike <revike@ya.ru>
2021-04-23 07:49:29 +0300

    hw_3

---
+++ hw_3/urls.py
@@ -0,0 +1,18 @@
+from datetime import date
+from views import Index, About
+
+
+def secret_front(request):
+    request['data'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/about/': About(),
+}
