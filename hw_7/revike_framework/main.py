1a4cef1d30832892a5da037071171a9f5e21ebb0
revike <revike@ya.ru>
2021-05-07 13:53:39 +0300

    hw_7

---
+++ hw_7/revike_framework/main.py
@@ -0,0 +1,55 @@
+import quopri
+
+from .requests import PostRequests, GetRequests
+
+
+class PageNotFound404:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE NOT FOUND'
+
+
+class Framework:
+
+    def __init__(self, routes_obj, fronts_obj):
+        self.routes_lst = routes_obj
+        self.fronts_lst = fronts_obj
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+
+        if not path.endswith('/'):
+            path = f'{path}/'
+
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = data
+            print(f'POST запрос: {Framework.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = request_params
+            print(f'Get-параметры: {request_params}')
+
+        if path in self.routes_lst:
+            view = self.routes_lst[path]
+        else:
+            view = PageNotFound404()
+
+        for front in self.fronts_lst:
+            front(request)
+
+        code, body = view(request)
+        start_response(code, [('Content-Type', 'text/html')])
+        return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for key, value in data.items():
+            val = bytes(value.replace('%', '=').replace('+', ' '), 'utf-8')
+            val_decode_str = quopri.decodestring(val).decode('utf-8')
+            new_data[key] = val_decode_str
+        return new_data
